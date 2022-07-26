f1f31cdb1613c4981b9d5e1cfa4fdd6936ddb34d
c3c1l1a <cesswangumukima@gmail.com>
2022-07-26 18:31:37 +0800

    Add call to SpaceX missions API

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,21 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import MissionItem from './MissionItem';
+import { fetchAllMissions } from '../../redux/missions/missions';
 import './missions.css';
 
-const Missions = () => (
-  <div className="missionsPage">
-    <table className="missions-table">
-      <tr className="missions-table-head">
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th>-</th>
-      </tr>
-      <MissionItem />
-      <MissionItem />
-      <MissionItem />
+const Missions = () => {
+  const dispatch = useDispatch();
 
-    </table>
-  </div>
-);
+  useEffect(() => async () => {
+    await dispatch(fetchAllMissions());
+  }, []);
 
+  return (
+    <div className="missionsPage">
+      <table className="missions-table">
+        <tr className="missions-table-head">
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>-</th>
+        </tr>
+        <MissionItem />
+        <MissionItem />
+        <MissionItem />
+
+      </table>
+    </div>
+  );
+};
 export default Missions;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,16 +1,29 @@
-const FETCH_ALL = 'categories/CHECK_STATUS';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import http from '../../services/missionsAPI';
+
+const FETCH_ALL = 'missions/FETCH_ALL';
+const FULFIL_FETCH_ALL = 'mission/FETCH_ALL/fulfilled';
 
 export default function missionsReducer(state = [], action) {
   switch (action.type) {
     case FETCH_ALL:
       return state;
+    case FULFIL_FETCH_ALL: {
+      console.log(action.payload);
+      return state;
+    }
     default:
       return state;
   }
 }
 
-export function fetchAll() {
-  return {
-    type: fetchAll,
-  };
-}
+export const fetchAllMissions = createAsyncThunk(
+  FETCH_ALL,
+  async (_, thunkAPI) => {
+    const response = await http.get();
+    thunkAPI.dispatch({
+      type: FULFIL_FETCH_ALL,
+      payload: response.data,
+    });
+  },
+);
