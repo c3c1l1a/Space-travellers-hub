1c2503edef5976ac1f8d8fed7f2a2a0883147d41
bruk19 <brukteshome21@gmail.com>
2022-07-27 16:49:39 +0300

    Add rocket configeration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import missionsReducer from './missions/missions';
+import rockets from './rockets/rockets';
 
 const rootReducer = combineReducers({
   missions: missionsReducer,
+
 });
 
 const store = configureStore({
   reducer: rootReducer,
+  rockets,
 });
 
 export default store;
