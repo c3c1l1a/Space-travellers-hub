85bd89b4cdb5948a552eb48899369fe94a13632f
c3c1l1a <cesswangumukima@gmail.com>
2022-07-27 20:47:44 +0800

    Uncomment rockets component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Nav from './components/navigation/Nav';
 import Profile from './components/profile/Profile';
 import Missions from './components/missions/Missions';
-// import Rockets from './components/rockets/Rockets';
+import Rockets from './components/rockets/Rockets';
 
 import './App.css';
 
@@ -16,7 +16,7 @@ function App() {
         <Nav />
       </header>
       <Routes>
-        {/* <Route path="/rockets" element={<Rockets />} /> */}
+        <Route path="/rockets" element={<Rockets />} />
         <Route path="/missions" element={<Missions missions={state.missions} />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
