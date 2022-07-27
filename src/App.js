e2ac462176cfc4f6a9bf9aabe5f0b765b627b2c4
bruk19 <brukteshome21@gmail.com>
2022-07-27 13:00:27 +0300

    Add spaceService Api links

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        header
+        <Nav />
       </header>
       <Profile />
       <Missions missions={state.missions} />
