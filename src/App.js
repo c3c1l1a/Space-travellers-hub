219b043cedf2412ea7e9825f96e18d27a2eb1890
c3c1l1a <cesswangumukima@gmail.com>
2022-07-27 20:30:12 +0800

    Add missions and profile page to navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 import { useSelector } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Nav from './components/navigation/Nav';
 import Profile from './components/profile/Profile';
 import Missions from './components/missions/Missions';
-import Rockets from './components/rockets/Rockets';
+//import Rockets from './components/rockets/Rockets';
 
 import './App.css';
 
@@ -15,9 +16,12 @@ function App() {
       <header className="App-header">
         <Nav />
       </header>
-      <Route path="/rockets" element={<Rockets />} />
-      <Profile />
-      <Missions missions={state.missions} />
+      <Routes>
+        {/*<Route path="/rockets" element={<Rockets />} />*/}
+        <Route path="/missions" element={<Missions missions={state.missions} />} />
+        <Route path="/profile" element={ <Profile />} />
+      </Routes>
+      
     </div>
   );
 }
