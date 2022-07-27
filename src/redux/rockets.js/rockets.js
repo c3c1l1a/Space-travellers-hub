87376e08aa8eed74640679be9e80726f2abd5ab5
bruk19 <brukteshome21@gmail.com>
2022-07-27 13:03:24 +0300

    Add implimantation for reservation

--- src/redux/rockets.js/rockets.js
+++ src/redux/rockets.js/rockets.js
@@ -15,3 +15,16 @@ export default function rockets(state = [], action = {}) {
       return state;
   }
 }
+
+export const getRockets = async (dispatch, getState) => {
+  const currRockets = getState().rockets;
+  if (currRockets.length === 0) {
+    const { data } = await SpacesxService.getRockets();
+    const rockets = data.map((rocket) => ({
+      name: rocket.rocket_name,
+      description: rocket.description,
+      flickr_images: rocket.flickr_images[0],
+    }));
+    dispatch({ type: ADD_ALL_ROCKETS, payload: rockets });
+  }
+};
