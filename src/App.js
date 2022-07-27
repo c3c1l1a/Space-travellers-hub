15dc23ac4d87bb45c9765c5545da25db1d82e2bd
c3c1l1a <cesswangumukima@gmail.com>
2022-07-27 20:41:18 +0800

    Uncomment rockets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-/* eslint-disable */
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Nav from './components/navigation/Nav';
 import Profile from './components/profile/Profile';
 import Missions from './components/missions/Missions';
-//import Rockets from './components/rockets/Rockets';
+// import Rockets from './components/rockets/Rockets';
 
 import './App.css';
 
@@ -17,11 +16,11 @@ function App() {
         <Nav />
       </header>
       <Routes>
-        {/*<Route path="/rockets" element={<Rockets />} />*/}
+        {/* <Route path="/rockets" element={<Rockets />} /> */}
         <Route path="/missions" element={<Missions missions={state.missions} />} />
-        <Route path="/profile" element={ <Profile />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-      
+
     </div>
   );
 }
