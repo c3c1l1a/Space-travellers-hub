27fa4c47fe9be7287f2190fe54875a11510e7fd8
bruk19 <brukteshome21@gmail.com>
2022-07-27 12:09:41 +0300

    Add basic implimantationfor navigation

--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import logo from '../../assets/logo.png';
+import './Nav.css';
 
 const Nav = () => {
-    const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
   const toggleMenu = () => {
     setOpenMenu((openMenu) => !openMenu);
   };
@@ -27,6 +28,7 @@ const Nav = () => {
       text: 'My Profile',
     },
   ];
+
   return (
     <nav className="navBar">
       <div className="logo-container">
@@ -45,6 +47,17 @@ const Nav = () => {
           </li>
         ))}
       </ul>
+
+      <ul className="mobile-navUl" style={{ transform: openMenu ? 'translateX(0)' : 'translateX(120%)' }}>
+        {links.map((link) => (
+          <li key={link.path}>
+            <NavLink to={link.path} className="nav-link mobile" onClick={() => toggleMenu()}>
+              <span>{link.text.toUpperCase()}</span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
