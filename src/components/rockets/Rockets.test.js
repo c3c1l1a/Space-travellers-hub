710b9fe3ec472dd0e1891d9425202e642e1ffd14
bruk19 <brukteshome21@gmail.com>
2022-07-28 12:35:52 +0300

    Add lists for testing

---
+++ src/components/rockets/Rockets.test.js
@@ -0,0 +1,39 @@
+
+import axios from '../../http-common';
+
+jest.mock('../../http-common');
+
+describe('Rockets Component', () => {
+  beforeEach(async () => {
+    const state = {
+      data: [
+        {
+          id: 1,
+          name: 'Falcon 1',
+          description: 'fast',
+          flickr_images: ['https://imgur.com/DaCfMsj.jpg'],
+        },
+        {
+          id: 2,
+          name: 'Falcon 9',
+          description: 'more fast',
+          flickr_images: ['https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg'],
+        },
+        {
+          id: 3,
+          name: 'Falcon Heavy',
+          description: 'even more fast',
+          flickr_images: ['https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg'],
+        },
+        {
+          id: 4,
+          name: 'Starship',
+          description: 'fast fast',
+          flickr_images: ['https://live.staticflickr.com/65535/48954138962_ee541e6755_b.jpg'],
+        },
+
+      ],
+    };
+    await axios.get.mockResolvedValue(state);
+  });
+});
