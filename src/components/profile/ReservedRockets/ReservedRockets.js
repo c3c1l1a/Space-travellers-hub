5c2896ca5a1ce052fe66b9391b54d2ba28748ce6
bruk19 <brukteshome21@gmail.com>
2022-07-28 13:25:25 +0300

    Add implemantation for reservedrockets

--- src/components/profile/ReservedRockets/ReservedRockets.js
+++ src/components/profile/ReservedRockets/ReservedRockets.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ReservedRockets = () => {
-    return (y
+    const reservedRockets = useSelector((state) => (
+        state.rockets.filter((rocket) => rocket.reserved)
 
+    return (
+        <div>
+        <h2>My Rockets</h2>
+        <ul className="list">
+          {reservedRockets.length > 0
+            ? reservedRockets.map((rocket) => (
+              <li className="list-item" key={rocket.id}>
+                {rocket.name}
+              </li>
+            ))
+            : 'You have no reserved rockets'}
+        </ul>
+      </div>
     );
 };
 
